b453fe8a44caa976e084542d25577b833b94aaa0
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-12-26 00:25:20 +0200

    added index & contacts

---
+++ contacts.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const fs = require('fs').promises;
+const { v4: uuid } = require('uuid');
+//  Розкоментуйте і запиши значення
+
+ const contactsPath = path.resolve('./db/contacts.json');;
+ 
+
+// TODO: задокументувати кожну функцію
+async function listContacts() {
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8');
+        const contactsList = JSON.parse(contactsData);
+        return contactsList;
+    } catch (error) {
+        console.error(error);
+    }
+}
+  
+async function getContactById(contactId) {
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8');
+        const contactsList = JSON.parse(contactsData);
+        const stringedContactId = contactId.toString();
+        const contactById = contactsList.find(
+            contact => contact.id === stringedContactId
+        );
+
+        return contactById || null;
+    } catch (error) {
+        console.error(error);
+    }
+}
+  
+async function removeContact(contactId) {
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8');
+        const contactsList = JSON.parse(contactsData);
+        const stringedContactId = contactId.toString();
+        const indexRemovedContact = contactsList.findIndex(
+            contact => contact.id === stringedContactId
+        );
+
+        if (indexRemovedContact === -1) {
+            return null;
+        }
+
+        const [removedContact] = contactsList.splice(indexRemovedContact, 1);
+        const stringedContactsList = JSON.stringify(contactsList);
+        await fs.writeFile(contactsPath, stringedContactsList, 'utf8');
+
+        return removedContact;
+    } catch (err) {
+        console.error(err);
+    }
+}
+  
+async function addContact(name, email, phone) {
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8');
+        const contactsList = JSON.parse(contactsData);
+        const newContact = { id: uuid(), name, email, phone };
+
+        contactsList.push(newContact);
+
+        const stringedContactsList = JSON.stringify(contactsList);
+
+        await fs.writeFile(contactsPath, stringedContactsList, 'utf8');
+
+        return newContact;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
